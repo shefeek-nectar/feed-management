4ecc1decc4e5e10dc789fd8f186479dbd20a31f2
shefeek-nectar <shefeek@nectarit.com>
2024-02-10 16:18:45 +0530

    added screen id

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  final String id = "homeScreen";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,23 +15,40 @@ class HomeScreen extends StatelessWidget {
         itemCount: 4,
         itemBuilder: (context, index) {
           return const Card(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      child: Icon(Icons.person),
-                    ),
-                    
-                    Column(children: [
-                      
-                      Text("Name"),
-                      
-                      
-                    ],)
-                  ],
-                )
-              ],
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        child: Icon(Icons.person),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "Name",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "3 hours ago",
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           );
         },
